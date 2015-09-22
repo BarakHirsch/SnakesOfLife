e5beef5bc892eb93b5c64305e50cd8d84431cf79
levin-g <gena.levin@gmail.com>
2015-09-22 14:25:07 +0300

    Added Screens and Navigation - Basic

--- SnakesOfLife/Web/www/components/snakes.js
+++ SnakesOfLife/Web/www/components/snakes.js
@@ -2,7 +2,31 @@
  * Angular.js SnakesOfLife Controller
  */
 
-angular.module('SnakesOfLife', [])
+angular.module('SnakesOfLife', ['ngRoute', 'ngAnimate'])
+    .config(function($routeProvider) {
+        $routeProvider
+
+            .when('/', {
+                templateUrl: 'main.html'
+            })
+
+            // route for the home page
+            .when('/simulate', {
+                templateUrl : 'simulate.html',
+                controller: 'SnakesOfLifeCtrl'
+            })
+
+            // route for the about page
+            .when('/research', {
+                templateUrl: 'research.html',
+                controller: 'researchController'
+            })
+
+            // route for the contact page
+            .when('/about', {
+                templateUrl: 'about.html'
+            });
+    })
     .controller('SnakesOfLifeCtrl', [
         '$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 
@@ -10,10 +34,13 @@ angular.module('SnakesOfLife', [])
 
             $scope.snakesNum = 0;
             $scope.turnsNum = 0;
+            $scope.stopping = false;
 
             $scope.grass = [['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live'], ['Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live', 'Live']];
 
             $scope.reset = function() {
+                $scope.stopping = true;
+                $scope.$apply();
                 $scope.gameGuid = null;
                 $scope.turnsNum = 0;
                 $scope.snakesNum = 0;
@@ -30,15 +57,17 @@ angular.module('SnakesOfLife', [])
                 var dataObj = {};
 
                 $http.put('https://snakesoflife.azurewebsites.net/api/run/' + $scope.gameGuid, dataObj).
-                    success(function(data) {
-                        $scope.turnsNum++;
-                        $scope.snakesNum = data.Snakes.length;
-                        //console.log(data.GrassCellsState);
-                        $scope.grass = data.GrassCellsState;
-                        if (data.HasEnded) {
-                            alert("Run Simulation Has Ended");
-                            $scope.gameGuid = null;
-                            $scope.started = false;
+                    success(function (data) {
+                        if (!$scope.stopping) {
+                            $scope.turnsNum++;
+                            $scope.snakesNum = data.Snakes.length;
+                            //console.log(data.GrassCellsState);
+                            $scope.grass = data.GrassCellsState;
+                            if (data.HasEnded) {
+                                alert("Run Simulation Has Ended");
+                                $scope.gameGuid = null;
+                                $scope.started = false;
+                            }
                         }
                     }).
                     error(function(data, status, headers, config) {
@@ -74,7 +103,7 @@ angular.module('SnakesOfLife', [])
                             $scope.gameGuid = data;
                             $scope.turnsNum = 0;
                             $scope.snakesNum = 0;
-
+                            $scope.stopping = false;
                         }).
                         error(function(data, status, headers, config) {
                             console.log("error ocurred - " + data);
